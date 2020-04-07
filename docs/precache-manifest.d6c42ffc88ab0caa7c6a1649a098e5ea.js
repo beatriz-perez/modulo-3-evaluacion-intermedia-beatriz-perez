808ed76ae7489bbb41220b4e98d49fa0c5ad95f8
beatriz-perez <beatriz.pfp@gmail.com>
2020-04-07 10:22:42 +0200

    add docs folder

---
+++ docs/precache-manifest.d6c42ffc88ab0caa7c6a1649a098e5ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db90cfafd640f39e636a4dc0a078b45",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e282009123cbfa6b4bd",
+    "url": "./static/css/main.e946a4f9.chunk.css"
+  },
+  {
+    "revision": "f2a383df2d9b53775d0e",
+    "url": "./static/js/2.b83d02c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b83d02c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e282009123cbfa6b4bd",
+    "url": "./static/js/main.fe8fffda.chunk.js"
+  },
+  {
+    "revision": "b534f079d1572efbf673",
+    "url": "./static/js/runtime-main.62779cc3.js"
+  }
+]);
